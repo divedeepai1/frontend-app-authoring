9c1c02b3c00485e8e0cf8df67511cee33d537310
EhtishamHaider <ehtisham.haider@divedeepai.com>
2025-12-02 11:43:53 +0500

    removed plus

--- src/compugrade/pages/MultiPartLessonBuilder/components/ProgrammaticErrorCodeModal.jsx
+++ src/compugrade/pages/MultiPartLessonBuilder/components/ProgrammaticErrorCodeModal.jsx
@@ -80,7 +80,7 @@ export default function ProgrammaticErrorCodeModal({
         return "";
       })
       .filter(Boolean)
-      .join(" + ");
+      .join(" ");
   };
 
   const handleSave = () => {
