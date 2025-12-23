01ecee11da205542970a36cfe0a2f0f248954fac
EhtishamHaider <ehtisham.haider@divedeepai.com>
2025-12-23 12:40:50 +0500

    spelling fix

--- src/compugrade-constants.js
+++ src/compugrade-constants.js
@@ -1,4 +1,4 @@
-export const base_url = "https://producation.wordaddin.educating.ai"
+export const base_url = "https://production.wordaddin.educating.ai"
 // export const base_url = "https://dassadas.fsdfafs.ai"
 
 // export const base_url = "https://37cf448022a4.ngrok-free.app"
