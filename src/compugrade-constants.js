5b2de5ed4f8ddd52b6b6ac6ae10d1db6233cefb6
divedeepai1 <154306980+divedeepai1@users.noreply.github.com>
2026-02-02 15:14:45 +0500

    Update compugrade-constants.js

--- src/compugrade-constants.js
+++ src/compugrade-constants.js
@@ -1,4 +1,4 @@
-export const base_url = "https://freetrail-staging.wordaddin.educating.ai/"
+export const base_url = "https://freetrail-staging.wordaddin.educating.ai"
 // export const base_url = "https://dassadas.fsdfafs.ai"
 
 // export const base_url = "https://37cf448022a4.ngrok-free.app"
