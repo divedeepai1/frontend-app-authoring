dacdd779f49828efc2609e759cbc72576ab04894
EhtishamHaider <ehtisham.haider@divedeepai.com>
2026-06-03 17:05:49 +0500

    changed base url

--- src/compugrade-constants.js
+++ src/compugrade-constants.js
@@ -1,4 +1,4 @@
-export const base_url = "https://yn4dpc5nkg.us-east-1.awsapprunner.com"
+export const base_url = "https://production.wordaddin.educating.ai"
 // export const base_url = "https://dassadas.fsdfafs.ai"
 
 // export const base_url = "https://37cf448022a4.ngrok-free.app"
